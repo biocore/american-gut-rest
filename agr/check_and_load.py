aa541f3cf2696993f000b286bdbba73e180fed2b
Daniel McDonald <mcdonadt@colorado.edu>
2015-07-28 15:23:26 -0600

    ENH: resolve #9, not doing HEAD

--- agr/check_and_load.py
+++ agr/check_and_load.py
@@ -38,6 +38,7 @@ def generate_per_sample_biom(biom_file, limit):
     table = load_table(biom_file)
     obs_ids = table.ids(axis='observation')
     obs_md = table.metadata(axis='observation')
+    fmt_taxonomy = lambda x: '; '.join(x)
 
     if limit is None:
         limit = np.inf
@@ -51,7 +52,8 @@ def generate_per_sample_biom(biom_file, limit):
         single_sample.filter(lambda v_, i, md: v_ > 0, axis='observation')
         biomv1 = single_sample.to_json('AG')
         biomtxt = single_sample.to_tsv(header_key='taxonomy',
-                                       header_value='taxonomy')
+                                       header_value='taxonomy',
+                                       metadata_formatter=fmt_taxonomy)
         yield (sample, biomv1, biomtxt)
         count += 1
 
@@ -155,13 +157,13 @@ if __name__ == '__main__':
     import sys
 
     if not database_connectivity():
-        sys.stderr.write("Cannot connect to the database")
+        sys.stderr.write("Cannot connect to the database\n")
         sys.exit(1)
     if not database_exists():
-        sys.stderr.write('Database does not exist')
+        sys.stderr.write('Database does not exist\n')
         sys.exit(1)
     if not schema_is_sane():
-        sys.stderr.write('Schema does not appear to be sane')
+        sys.stderr.write('Schema does not appear to be sane\n')
         sys.exit(1)
 
     c = connect(user=agr.db_user, host=agr.db_host, password=agr.db_password,
@@ -169,9 +171,9 @@ if __name__ == '__main__':
     c.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cur = c.cursor()
 
-    #if biom_unchanged(cur):
+    if biom_unchanged(cur):
         # data are the same, no change
-    #    sys.exit(0)
+        sys.exit(0)
 
     do_biom_update(cur)
     do_fq_update(cur)

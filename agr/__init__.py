e39e814ba72cb5870fe8fc9544d35e3d27795803
Daniel McDonald <mcdonadt@colorado.edu>
2015-08-07 11:29:44 -0600

    DOC/ENH     * expanded descriptions of the different confs     * can write out http and server config separately     * support for a admin db role

--- agr/__init__.py
+++ agr/__init__.py
@@ -12,14 +12,40 @@ from ConfigParser import ConfigParser
 
 # configuration defaults
 _defaults = {
+    # the server port is the port that the nginx webserver will listen on
+    'serverport': '8080',
+
+    # the base_conf_dir is the location for the nginx server config.
+    # NOTE: there are multiple config files for nginx, the primary one being
+    # the "http" config. The base_conf_dir houses other more specific configs
+    # and in the case of american gut rest, it houses the specific "server"
+    # config which includes details on the endpoints etc.
+    'base_conf_dir': '.',
+
+    # the db_* credentials are for READ access with the database
     'db_user': 'postgres',
     'db_host': 'localhost',
     'db_password': '',
     'db_name': 'ag_rest',
+
+    # the admin_db_* credentials are for WRITE access with the database
+    # NOTE: db_name and db_host are not replicated
+    'admin_db_user': 'postgres',
+    'admin_db_password': '',
+
+    # Indicate if we're operating in a test_environment.
+    # WARNING: if test_environment is True, then it is possible to drop the
+    # database
     'test_environment': True,
+
+    # ag_*_src configs are for the data sources themselves, these must be
+    # URLs that can be retreived using requests.get
     'ag_biom_src': 'https://github.com/biocore/American-Gut/blob/master/data/AG/AG_100nt_even10k.biom?raw=true',  # noqa
-    'ag_biom_src_api': 'https://api.github.com/repos/biocore/American-Gut/commits?path=data/AG/AG.biom',  # noqa
-    'ag_accession_src': 'https://github.com/biocore/American-Gut/blob/master/data/AG/accession_to_sample.json?raw=true'  # noqa
+    'ag_accession_src': 'https://github.com/biocore/American-Gut/blob/master/data/AG/accession_to_sample.json?raw=true',  # noqa
+
+    # the ag_*api configs are for the specific RESTful interfaces pinged to
+    # determine if there have been changes to the corresponding src files
+    'ag_biom_src_api': 'https://api.github.com/repos/biocore/American-Gut/commits?path=data/AG/AG.biom'  # noqa
 }
 
 
@@ -38,16 +64,20 @@ else:
 
 
 # set the configuration variables
+serverport = get('serverport')
+base_conf_dir = get('base_conf_dir')
 db_user = get('db_user')
 db_host = get('db_host')
 db_password = get('db_password')
 db_name = get('db_name')
+admin_db_user = get('admin_db_user')
+admin_db_password = get('admin_db_password')
 test_environment = getboolean('test_environment')
 ag_biom_src = get('ag_biom_src')
 ag_biom_src_api = get('ag_biom_src_api')
 ag_accession_src = get('ag_accession_src')
 
 
-__all__ = ['db_user', 'db_hostname', 'db_password', 'db_name',
-           'test_environment', 'ag_biom_src', 'ag_biom_src_api',
-           'ag_accession_src']
+__all__ = ['serverport', 'db_user', 'db_hostname', 'db_password', 'db_name',
+           'admin_db_user', 'admin_db_password', 'test_environment',
+           'ag_biom_src', 'ag_biom_src_api', 'ag_accession_src']
--- scripts/nginx_config.py
+++ scripts/nginx_config.py
@@ -9,22 +9,56 @@
 # ----------------------------------------------------------------------------
 
 import os
+import sys
 import agr
 
 
-def get_nginx_template():
-    """Source the nginx template"""
+def get_base():
+    """Get the base support file configuration directory"""
     cwd = os.path.join(os.path.dirname(os.path.abspath(__file__)))
     supp_files = '../agr/support_files'
-    conf_template = os.path.join(cwd, supp_files, 'nginx_config.template')
+    return os.path.join(cwd, supp_files)
+
+
+def get_server_template():
+    """Source the nginx server template"""
+    conf_template = os.path.join(get_base(), 'server_config.template')
+    with open(os.path.join(conf_template)) as fp:
+        return fp.read()
+
+
+def get_http_template():
+    """Source the nginx http template"""
+    conf_template = os.path.join(get_base(), 'http_config.template')
     with open(os.path.join(conf_template)) as fp:
         return fp.read()
 
 
 if __name__ == '__main__':
-    template = get_nginx_template()
+    if len(sys.argv) != 2:
+        usage = "usage: %s [http|server]\n" % sys.argv[0]
+        sys.stderr.write(usage)
+        sys.exit(1)
 
-    print template % {'db_user': agr.db_user,
-                      'db_name': agr.db_name,
-                      'db_password': agr.db_password,
-                      'db_host': agr.db_host}
+    if sys.argv[1] == 'http':
+        template = get_http_template()
+        populated = template % {'base_conf_dir': agr.base_conf_dir,
+                                'db_user': agr.db_user,
+                                'db_name': agr.db_name,
+                                'db_password': agr.db_password,
+                                'db_host': agr.db_host}
+        filename = os.path.join(agr.base_conf_dir, 'nginx.conf')
+    elif sys.argv[1] == 'server':
+        template = get_server_template()
+        populated = template % {'serverport': agr.serverport}
+        filename = os.path.join(agr.base_conf_dir, 'agr.apiserver.conf')
+    else:
+        sys.stderr.write("Unknown argument: %s\n" % sys.argv[1])
+        exit(1)
+
+    if os.path.exists(filename):
+        sys.stderr.write("%s already exists\n" % filename)
+        sys.exit(1)
+
+    with open(filename, 'w') as fp:
+        fp.write(populated)
